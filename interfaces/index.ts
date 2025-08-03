6136b8e8d378ad7f94f113b673fa25af371606e2
idriss <ssenuoy.issiurd@gmail.com>
2025-08-03 20:15:05 +0100

    sgseg

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,34 @@
+// interfaces/index.ts
 
-
-function index() {
-
+export interface AddressProps {
+  city: string;
+  country: string;
 }
 
-export default index
+export interface ReviewProps {
+  avatar: string;
+  name: string;
+  rating: number;
+  comment: string;
+}
+
+export interface HostProps {
+  name: string;
+  avatar: string;
+  about: string;
+}
+
+export interface PropertyProps {
+  id: string;
+  name: string;
+  rating: number;
+  reviewsCount: number;
+  address: AddressProps;
+  image: string; // The main image
+  images: string[]; // A list of additional images for the grid
+  description: string;
+  category: string[]; // Represents amenities
+  price: number;
+  reviews: ReviewProps[];
+  host: HostProps;
+}
